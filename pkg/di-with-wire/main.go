c805155c4aecdcb75e3fdad8110f2613ae97766e
albuquerque53 <gabrielalbuquerque-dev@hotmail.com>
2022-05-21 16:01:50 -0300

    feat: simple example using wire DI

---
+++ pkg/di-with-wire/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Message represents the greet message
+type Message string
+
+// Greeter must greet the others with a specific message
+type Greeter struct {
+	Message Message
+}
+
+// Event is the hook for domain, must execute the logic
+type Event struct {
+	Greeter Greeter
+}
+
+// NewMessage returns the message "Hello, world!"
+func NewMessage() Message {
+	return Message("Hello, world!")
+}
+
+// NewGreeter receives a Message that are used to instance a new Gretter, returns the Greeter
+func NewGreeter(m Message) Greeter {
+	return Greeter{Message: m}
+}
+
+// Greet is a method of Greeter, must return the Message
+func (g Greeter) Greet() Message {
+	return g.Message
+}
+
+// NewEvent receives a Greeter that are used to instance a new Event, returns the Event
+func NewEvent(g Greeter) Event {
+	return Event{Greeter: g}
+}
+
+// Start is a method of Event, must execute the bussiness logic
+func (e Event) Start() {
+	msg := e.Greeter.Greet()
+	fmt.Println(msg)
+}
+
+// main is the entrypoint of application, will instance the application
+func main() {
+	event := InitializeEvent()
+
+	event.Start()
+}
